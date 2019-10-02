2ec50532a419234b6177fedb8b14a625eb5cfe8b
Kaitlyn Reaser <reaserkaitlyn@gmail.com>
2019-10-02 19:22:41 -0400

    XD

--- Week02/ClassesIntro/ClassesIntro/Dog.cs
+++ Week02/ClassesIntro/ClassesIntro/Dog.cs
@@ -12,19 +12,34 @@ namespace ClassesIntro
         public string Breed;
         public int Height;
         public string Name;
+        public string Random_Sound;
+        private string v1;
+        private int v2;
+        private string dogName;
 
         public void Bark()
         {
-            MessageBox.Show("Bark");
+            MessageBox.Show("*Barks in French*");
+        }
+        public void SayName()
+        {
+            MessageBox.Show("Hello, my name is " + Name + ".");
         }
 
-        public Dog() { }
 
-        public Dog(string breed, int height, string name)
+        public Dog(string breed, int height, string name, string random_sound)
         {
             this.Breed = breed;
             this.Height = height;
             this.Name = name;
+            this.Random_Sound = random_sound;
+        }
+
+        public Dog(string v1, int v2, string dogName)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.dogName = dogName;
         }
     }
 }
--- Week02/ClassesIntro/ClassesIntro/MainWindow.xaml.cs
+++ Week02/ClassesIntro/ClassesIntro/MainWindow.xaml.cs
@@ -36,7 +36,26 @@ namespace ClassesIntro
             string dogName = DogName.Text;
 
             Dog dog = new Dog("Corgi", 12, dogName);
-            dog.SayName();
+          dog.SayName();
         }
+
+        public string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+
+
+
     }
+
 }
